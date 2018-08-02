a6679b168a8e2916cd99c5d0c996829c87773556
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-08-02 21:34:29 +0300

    Started front ends for workouts and challenges
    Notice the new subfolder structure for different app sections. The Generate-component should be also moved to Generate-folder.
    And renamed to Generate. Then we could create a cool frontpage. Just a static, cool looking start page with the same top menu. Probably just a big Evolve App -logo.
    
    - Created Workouts-page
    - Created Challenges-page
    - Tweaked About-page.

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Workouts from "./components/Workouts/Workouts";
+import Challenges from "./components/Challenges/Challenges";
 import Frontpage from "./components/Frontpage";
 import About from "./components/About";
 
 export default class Routes extends React.PureComponent {
   render() {
     return (
-      <Router basename="/evolve"> 
+      <Router>
         <Switch>
+          <Route path="/Workouts/Workouts" component={Workouts} exact />
+          <Route path="/Challenges/Challenges" component={Challenges} exact />
           <Route path="/" component={Frontpage} exact />
           <Route path="/About" component={About} exact />
         </Switch>
---
+++ src/components/Workouts/WorkoutFilters.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import { connectContext } from "react-connect-context";
+import { Context } from "../../context";
+
+class Filters extends Component {
+  toggleButton = value => {
+    this.props.updateFilters(value);
+  };
+
+  checkActive = value => {
+    const { filters } = this.props;
+    return filters.indexOf(value) > -1;
+  };
+
+  renderButton = text => {
+    return (
+      <Button
+        active={this.checkActive(text)}
+        onClick={() => this.toggleButton(text)}
+        content={text}
+      />
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <br />
+        <Button.Group widths="3" size="small">
+          {this.renderButton("Calisthenics")}
+          {this.renderButton("Gym")}
+          {this.renderButton("Mixed")}
+        </Button.Group>
+        <br /> <br />
+        <Button.Group widths="4" size="small">
+          {this.renderButton("Upper body")}
+          {this.renderButton("Lower body")}
+          {this.renderButton("Full body")}
+          {this.renderButton("Core")}
+        </Button.Group>
+        <br /> <br />
+        <Button.Group widths="3" size="small">
+          {this.renderButton("Beginner")}
+          {this.renderButton("Intermediate")}
+          {this.renderButton("Advanced")}
+        </Button.Group>
+        <br />
+        <br />
+      </div>
+    );
+  }
+}
+
+export default connectContext(Context)(Filters);
