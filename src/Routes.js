f40621a02f358beea05feb0361c18f8fab67f90c
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-08-02 23:38:41 +0300

    Add files via upload
    Prevoius pull request closed. Made few extra modifications already.
    Notice the new subfolder structure for different app sections.
    
    Created Workouts-page with dummy layout
    Created Challenges-page with dummy layout
    Tweaked About-page.
    Renamed Generate-component from frontpage to Generate and created frontpage base.

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Frontpage from "./components/Frontpage";
+import Workouts from "./components/Workouts/Workouts";
+import Challenges from "./components/Challenges/Challenges";
+import Generate from "./components/Generate/Generate";
 import About from "./components/About";
 
 export default class Routes extends React.PureComponent {
   render() {
     return (
-      <Router basename="/evolve"> 
+      <Router>
         <Switch>
           <Route path="/" component={Frontpage} exact />
+          <Route path="/Workouts/Workouts" component={Workouts} exact />
+          <Route path="/Challenges/Challenges" component={Challenges} exact />
+          <Route path="/Generate/Generate" component={Generate} exact />
           <Route path="/About" component={About} exact />
         </Switch>
       </Router>
---
+++ src/components/Generate/GeneratedWorkout.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button, Table } from "semantic-ui-react";
+
+class GeneratedWorkout extends React.PureComponent {
+  renderRows = () => {
+    return this.props.workouts.map(item => {
+      return (
+        <Table.Row key={item.name}>
+          <Table.Cell>{item.name}</Table.Cell>
+          <Table.Cell>{Math.floor(Math.random() * 11) + 5}</Table.Cell>
+          <Table.Cell>
+            <Button.Group compact size="mini">
+              <Button icon="minus" />
+              <Button icon="plus" />
+            </Button.Group>
+          </Table.Cell>
+        </Table.Row>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <Table basic="very" unstackable selectable celled fixed compact>
+        <Table.Header>
+          <Table.Row textAlign="center">
+            <Table.HeaderCell width={9}>Exercise</Table.HeaderCell>
+            <Table.HeaderCell width={3}>Reps</Table.HeaderCell>
+            <Table.HeaderCell width={4}>Edit</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{this.renderRows()}</Table.Body>
+      </Table>
+    );
+  }
+}
+
+export default GeneratedWorkout;
