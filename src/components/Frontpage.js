572eed848afabcc2829bcaf81d24c32aa89e45ed
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-08-04 12:30:16 +0300

    Frontpage layout. Some trouble still in getting the background image css-properties correct.

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,17 +1,44 @@
 import React from "react";
 import { connectContext } from "react-connect-context";
 import { Context } from "../context";
-import Layout from "./Layout";
-import { Header, Segment } from "semantic-ui-react";
+import Menu from "./Menu";
+import Footer from "./Footer";
+import { Header, Image } from "semantic-ui-react";
+import CoverImage from "../img/Frontpage.jpeg";
+
+const bg = {
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  opacity: 0.6,
+  overflow: "hidden"
+};
+
+const textStyle = {
+  width: 220,
+  height: 50,
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  margin: "auto"
+};
 
 class Frontpage extends React.PureComponent {
   render() {
     return (
-      <Layout {...this.props}>
-        <Segment inverted color="teal">
-          <Header as="h2" content="Evolve App" textAlign="center" />
-        </Segment>
-      </Layout>
+      <React.Fragment>
+        <Menu {...this.props} />
+        <Image src={CoverImage} style={bg} alt="bg" />
+        <Header
+          as="h1"
+          content="Evolve App"
+          textAlign="center"
+          inverted
+          style={textStyle}
+        />
+        <Footer />
+      </React.Fragment>
     );
   }
 }
