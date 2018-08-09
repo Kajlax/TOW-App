ffe96017b89073dcebaf05d5ad5e91d59a55a223
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-08-09 10:02:38 +0300

    Removed rep-scaling from filters-pane. Scaling is done in WorkoutsetComponent.js

--- src/components/Workouts/WorkoutFilters.js
+++ src/components/Workouts/WorkoutFilters.js
@@ -23,17 +23,6 @@ class Filters extends Component {
     );
   };
 
-  renderDifficultyButton = (text, color) => {
-    return (
-      <Button
-        active={this.checkActive(text)}
-        onClick={() => this.toggleButton(text, color)}
-        content={text}
-        color={color}
-      />
-    );
-  };
-
   render() {
     return (
       <div>
@@ -43,19 +32,13 @@ class Filters extends Component {
           {this.renderButton("Gym")}
           {this.renderButton("Mixed")}
         </Button.Group>
-        <br /> <br />
+        <br />
+        <br />
         <Button.Group widths="4" size="small">
           {this.renderButton("Upper body")}
           {this.renderButton("Lower body")}
           {this.renderButton("Full body")}
           {this.renderButton("Core")}
-        </Button.Group>
-        <br /> <br />
-        <Button.Group widths="4" size="small">
-          {this.renderDifficultyButton("25 %", "green")}
-          {this.renderDifficultyButton("50 %", "yellow")}
-          {this.renderDifficultyButton("75 %", "orange")}
-          {this.renderDifficultyButton("100 %", "red")}
         </Button.Group>
       </div>
     );
