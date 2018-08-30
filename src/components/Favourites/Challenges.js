5a2826d540f205a246fe0941382ec1a3320443b0
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-08-30 17:13:37 +0300

    Started working on favourite-Challenge rendering

--- src/components/Favourites/Challenges.js
+++ src/components/Favourites/Challenges.js
@@ -1,22 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import ChallengeActions from "../../redux/reducers/challengeRedux";
+import Loading from "../Loading";
 
-export default class Challenges extends React.PureComponent {
-  renderRows = () => {
+class Challenges extends React.Component {
+  componentDidMount() {
+    const { challenges, getChallenges } = this.props;
+    if (challenges.length === 0) {
+      getChallenges();
+    }
+  }
+
+  renderComponent = () => {
     const { challenges } = this.props;
-    return challenges.map((row, i) => {
-      if (i !== 0) {
-        return (
-          <div key={row}>
-            <Link to={`/challenges/${row}`}>{row}</Link>
-          </div>
-        );
-      }
-      return null;
-    });
+
+    return (
+      <div>
+        <Link to={`/challenges/${challenges.id}`}>{challenges[0].name}</Link>
+      </div>
+    );
   };
 
   render() {
-    return <div>{this.renderRows()}</div>;
+    const { fetching, challenges } = this.props;
+    return (
+      <div>
+        {!fetching && challenges.length > 0 ? (
+          this.renderComponent()
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
   }
 }
+
+const mapStateToProps = state => ({
+  challenges: state.challenge.challenges,
+  fetching: state.challenge.fetching,
+  error: state.challenge.error
+});
+
+const mapDispatchToProps = dispatch => ({
+  getChallenges: () => dispatch(ChallengeActions.fetchChallenges())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Challenges);
