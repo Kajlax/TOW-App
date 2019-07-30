70c469e404a5e234942ba069cc7aee778ea470a6
Lauri Katajisto <lauri.katajisto@geniem.com>
2019-07-30 18:31:29 +0300

    update devtools.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import reducers from './reducers';
-import sagas from './sagas';
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import reducers from "./reducers";
+import sagas from "./sagas";
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducers,
   compose(
     applyMiddleware(sagaMiddleware),
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
-  ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 sagaMiddleware.run(sagas);
 
